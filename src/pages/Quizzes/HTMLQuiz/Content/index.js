83f0a4d753aa0c57861776bbe317454d6872b661
Stephan Krug <stekrug@icloud.com>
2021-12-22 15:53:55 -0300

    Fix Buttons

--- src/pages/Quizzes/HTMLQuiz/Content/index.js
+++ src/pages/Quizzes/HTMLQuiz/Content/index.js
@@ -3,15 +3,27 @@ import { Text, View, TextInput, TouchableOpacity, StatusBar } from 'react-native
 
 import styles from './style'
 export default function Quizzes() {
+  const first = '<h5>'
+  const second = '<p>'
+  const third = '<h1>'
+
   return (
     <View>
         <Text>1 de 1</Text>
         <Text>Qual tag é usada para fazer títulos grandes?</Text>
         <Image
         source={'./images/Text Question Image.png'}></Image>
-        <TouchableOpacity>A.  </TouchableOpacity>
-        <TouchableOpacity>B.  </TouchableOpacity>
-        <TouchableOpacity>C.  </TouchableOpacity>
+        <TouchableOpacity
+        onPress={}
+        >
+          A. {first} 
+        </TouchableOpacity>
+        <TouchableOpacity>
+          B. {second}
+        </TouchableOpacity>
+        <TouchableOpacity>
+          C. {third}
+        </TouchableOpacity>
         <TouchableOpacity>Continuar</TouchableOpacity>
     </View>
       
