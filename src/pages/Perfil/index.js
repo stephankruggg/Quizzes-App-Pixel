0b965d77404395faec64a151e2ae993aa7f26a32
Stephan Krug <stekrug@icloud.com>
2021-12-30 19:31:47 -0300

    Fix Profile

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView, Text, Image, TouchableOpacity, View } from 'react-native';
 import styles from './style';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Profile({navigation}) {
   return (
-    <View>
-      <Text style={styles.Title}>Profile</Text>
-      <TouchableOpacity style={styles.HistButton}><Text>Seu histórico</Text></TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('About')} style={styles.AboutButton}><Text>Sobre o app</Text></TouchableOpacity>
-    </View>
+    <SafeAreaView>
+      <View style={styles.Visualization}>
+        <TouchableOpacity onPress={() => navigation.navigate('Home', {screen: 'Quizzes'})} style={styles.Back}>
+          <AntDesign name="left" size={15} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.Title}>Profile</Text>
+      </View>
+      <Image
+        style={styles.Avatar}
+        source={{
+            uri:'./images/Avatar.png'
+        }}
+        ></Image>
+      <TouchableOpacity style={styles.HistButton}>
+        <Text style={styles.ButtonText}>Seu histórico</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('About')} style={styles.AboutButton}>
+        <Text style={styles.ButtonText}>Sobre o app</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
--- src/pages/Perfil/style.js
+++ src/pages/Perfil/style.js
@@ -1,35 +1,67 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
+    Visualization: {
+        flexDirection: 'row'
+    },
+    Back: {
+        borderRadius: 50,
+        height: 40,
+        width: 40,
+        marginLeft: 16,
+        marginTop: 20,
+        borderColor: '#BEBAB3',
+        borderWidth: 1,
+        alignContent: 'center',
+        alignItems: 'center',
+        paddingTop: 11
+    },
     Title: {
-        width: '100%',
+        width: 232,
         height: 32,
         fontSize: 24,
-        alignContent: 'center'
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginLeft: 17,
+        marginTop: 25,
+
     },
     Avatar: {
-        marginTop: 61,
-        alignContent: 'center'
+        marginTop: 52,
+        marginLeft: 117,
+        borderWidth: 4,
+        borderColor: '#65AAEA',
+        borderRadius: 80,
+        alignContent: 'center',
+        width: 140,
+        height: 140
     },
     HistButton: {
         width: 343,
         height: 80,
-        alignItems: 'center',
-        alignContent: 'center',
-        paddingLeft: 16,
-        paddingTop: 24,
         borderRadius: 16,
-        marginTop: 32
+        borderWidth: 1,
+        marginTop: 32,
+        marginLeft: 16,
+        borderColor: '#BEBAB3'
     },
     AboutButton: {
         width: 343,
         height: 80,
-        alignItems: 'center',
-        alignContent: 'center',
-        paddingLeft: 16,
-        paddingTop: 24,
         borderRadius: 16,
-        marginTop: 16
+        borderWidth: 1,
+        marginTop: 16,
+        marginLeft: 16,
+        borderColor: '#BEBAB3'
+    },
+    ButtonText: {
+        marginLeft: 16,
+        marginTop: 24,
+        fontSize: 24,
+        fontWeight: 'bold',
+        width: 311,
+        height: 32,
+        textAlign: 'center'
     }
 });
 
